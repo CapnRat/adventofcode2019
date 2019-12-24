d4f165e129790eef08d599e1f4c06c002ba753bf
Shawn White <shawn@capnrat.com>
2019-12-24 04:41:40 -0700

    star_10_1 solution

--- star_10_1/solver.go
+++ star_10_1/solver.go
@@ -4,15 +4,16 @@ import (
 	"bufio"
 	"fmt"
 	"log"
+	"math/big"
 	"os"
 )
 
 const (
-	Empty    byte = '.'
-	Asteroid byte = '#'
+	CharEmpty    byte = '.'
+	CharAsteroid byte = '#'
 )
 
-const file = "star_10_1/file"
+const file = "star_10_1/input"
 
 type AsteroidField map[SpacePoint]bool
 
@@ -21,21 +22,74 @@ type SpacePoint struct{
 	y int
 }
 
+type Asteroid struct{
+	Point SpacePoint
+	VisibleAsteroids int
+}
+
 type Solver struct{}
 
 func (s *Solver) Solve() string {
-	asteroid, count := SolveWithInputFile(file)
-	return fmt.Sprintf("%d, %d (%d)", asteroid.x, asteroid.y, count)
+	asteroid := SolveWithInputFile(file)
+	return fmt.Sprintf("%d, %d (%d)", asteroid.Point.x, asteroid.Point.y, asteroid.VisibleAsteroids)
 }
 
-func SolveWithInputFile(path string) (SpacePoint, int) {
+func SolveWithInputFile(path string) Asteroid {
 	field := ParseAsteroidField(path)
 
-	for asteroid := range *field {
-		
+	var best Asteroid
+	for point := range *field {
+		type radial struct{
+			x int64
+			y int64
+		}
+		radials := make(map[radial]int)
+
+		for target := range *field {
+			relativeX := target.x - point.x
+			relativeY := target.y - point.y
+
+			if relativeX == 0 && relativeY == 0 {
+				continue
+			}
+
+			var rad radial
+			if relativeX == 0 {
+				rad = radial {0, int64(Sign(relativeY))}
+			} else if relativeY == 0 {
+				rad = radial {int64(Sign(relativeX)), 0}
+			} else {
+				a := big.NewInt(int64(relativeX))
+				b := big.NewInt(int64(relativeY))
+				absA := new(big.Int)
+				absB := new(big.Int)
+				absA.Abs(a)
+				absB.Abs(b)
+				var gcd big.Int
+				gcd.GCD(nil, nil, absA, absB)
+				rad = radial{a.Div(a, &gcd).Int64(), b.Div(b, &gcd).Int64()}
+			}
+
+			radials[rad]++
+		}
+
+		visible := len(radials)
+		if best.VisibleAsteroids < visible {
+			best.VisibleAsteroids = visible
+			best.Point = point
+		}
 	}
 
-	return SpacePoint{}, 0
+	return best
+}
+
+func Sign(a int) int {
+	if a > 0 {
+		return 1
+	} else if a < 0 {
+		return -1
+	}
+	return 0
 }
 
 func ParseAsteroidField(path string) *AsteroidField {
@@ -51,10 +105,11 @@ func ParseAsteroidField(path string) *AsteroidField {
 	for scanner.Scan() {
 		for x, b := range scanner.Bytes() {
 			switch b {
-			case Asteroid:
+			case CharAsteroid:
 				field[SpacePoint{x,y}] = true
 			}
 		}
+		y++
 	}
 
 	return &field
